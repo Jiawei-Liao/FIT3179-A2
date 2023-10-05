2ac6bbf4c5846fc96066f97583b8a70ec873a8a5
Jiawei Liao <jlia0058@student.monash.edu>
2023-10-05 13:55:07 +1100

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var spec1 = "js/choropleth.vg.json";
+vegaEmbed("#choropleth", spec1)
+    .then(function(result) {
+}).catch(console.error);
+
+var spec2 = "js/rank.vg.json";
+vegaEmbed("#rank", spec2)
+    .then(function(result) {
+}).catch(console.error);
+
+var spec3 = "js/scatter.vg.json";
+vegaEmbed("#scatter", spec3)
+    .then(function(result) {
+}).catch(console.error);
+
+var spec4 = "js/multiple_line.vg.json";
+vegaEmbed("#multiple_line", spec4)
+    .then(function(result) {
+}).catch(console.error);
+
+var spec5 = "js/line.vg.json";
+vegaEmbed("#line", spec5)
+    .then(function(result) {
+}).catch(console.error);
