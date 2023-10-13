9fc88c6062daf1f510c96d2a86b00db08adce5ca
Jiawei Liao <jlia0058@student.monash.edu>
2023-10-13 11:40:06 +1100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 var spec1 = "js/choropleth.vg.json";
-vegaEmbed("#choropleth", spec1)
+vegaEmbed("#choropleth", spec1, {actions: false})
     .then(function(result) {
 }).catch(console.error);
 
 var spec2 = "js/rank.vg.json";
-vegaEmbed("#rank", spec2)
+vegaEmbed("#rank", spec2, {actions: false})
     .then(function(result) {
 }).catch(console.error);
 
 var spec3 = "js/scatter.vg.json";
-vegaEmbed("#scatter", spec3)
+vegaEmbed("#scatter", spec3, {actions: false})
     .then(function(result) {
 }).catch(console.error);
 
 var spec4 = "js/multiple_line.vg.json";
-vegaEmbed("#multiple_line", spec4)
+vegaEmbed("#multiple_line", spec4, {actions: false})
     .then(function(result) {
 }).catch(console.error);
 
 var spec5 = "js/line.vg.json";
-vegaEmbed("#line", spec5)
+vegaEmbed("#line", spec5, {actions: false})
     .then(function(result) {
 }).catch(console.error);
